9ca8865e944430e131e889e534587bf48fbbe0b7
Uadson Feitosa <uadsonpy@gmail.com>
2023-02-26 13:23:53 -0300

    update main and requirement files

--- main.py
+++ main.py
@@ -1,19 +1,14 @@
 import uvicorn
+
 from fastapi import FastAPI
 
 app = FastAPI()
-
-
-@app.get("/")
-async def root():
-    return {"msg": "Welcome to the FastAPI"}
-
 
 if __name__ == "__main__":
     uvicorn.run(
         "main:app",
         host="0.0.0.0",
         port=8000,
-        log_level="info",
         reload=True
     )
+

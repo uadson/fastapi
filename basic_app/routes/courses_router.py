2b4dba5a4398d963cb854de9d307a230d5d4b8b1
Uadson Feitosa <uadsonpy@gmail.com>
2023-03-31 14:03:49 -0300

    basic_app deleted

--- basic_app/routes/courses_router.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter()
-
-@router.get('/api/v1/courses')
-async def get_courses():
-    return {"info": "All Courses"}
--- basic_app/routes/users_router.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-
-router = APIRouter()
-
-@router.get('/api/v1/users')
-async def get_users():
-    return {"info": "All Users"}

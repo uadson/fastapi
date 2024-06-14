4c05cef82e1aea1edf11c7157a69beaa8ba3cfda
Uadson Feitosa <uadsonpy@gmail.com>
2024-06-14 16:02:02 -0300

    implementação de relacionamento entre varios modelos

--- models_relationship/src/main.py
+++ models_relationship/src/main.py
@@ -41,6 +41,7 @@ class HeroBase(SQLModel):
 class Hero(HeroBase, table=True):
     id: int | None = Field(default=None, primary_key=True)
     hashed_password: str = Field()
+    
     team: Team | None = Relationship(back_populates='heroes')
     
 
@@ -59,6 +60,15 @@ class HeroUpdate(SQLModel):
     password: str | None = None
     team_id: int | None = None
 
+
+class HeroPublicWithTeam(HeroPublic):
+    team: TeamPublic | None = None
+    
+
+class TeamPublicWithHeroes(TeamPublic):
+    heroes: list[HeroPublic] = []
+    
+    
 # DATABASE
 sqlite_file_name = "db.sqlite3"
 sqlite_url = f"sqlite:///{sqlite_file_name}"
@@ -93,20 +103,6 @@ def on_startup():
 # ROUTERS
 
 # CREATE
-
-# NO DEPENDS
-# @app.post("/heroes/", response_model=HeroPublic, status_code=status.HTTP_201_CREATED)
-# def create_hero(hero: HeroCreate):
-#     hashed_password = hash_password(hero.password)
-#     with Session(engine) as session:
-#         extra_data = {'hashed_password': hashed_password}
-#         db_hero = Hero.model_validate(hero, update=extra_data)
-#         session.add(db_hero)
-#         session.commit()
-#         session.refresh(db_hero)
-#         return db_hero
-
-# WITH DEPENDS
 @app.post('/heroes/', response_model= HeroPublic, status_code=status.HTTP_201_CREATED)
 def create_hero(*, session: Session = Depends(get_session), hero: HeroCreate):
     hashed_password = hash_password(hero.password)
@@ -118,40 +114,14 @@ def create_hero(*, session: Session = Depends(get_session), hero: HeroCreate):
     return db_hero
 
 
-# @app.get("/heroes/", response_model=list[HeroPublic], status_code=status.HTTP_200_OK)
-# def read_heroes():
-#     with Session(engine) as session:
-#         heroes = session.exec(select(Hero)).all()
-#         return heroes
-
 # READ
-
-# NO DEPENDS
-# @app.get("/heroes/", response_model=list[HeroPublic], status_code=status.HTTP_200_OK)
-# def read_heroes(offset: int = 0, limit: int = Query(default=100, le=100)):
-#     with Session(engine) as session:
-#         heroes = session.exec(select(Hero).offset(offset).limit(limit)).all()
-#         return heroes
-
-# WITH DEPENDS
 @app.get('/heroes/', response_model=list[HeroPublic], status_code=status.HTTP_200_OK)
 def read_heroes(session: Session = Depends(get_session), offset: int = 0, limit: int = Query(default=100, le=100)):
     heroes = session.exec(select(Hero).offset(offset).limit(limit)).all()
     return heroes
 
-# READ
 
-# NO DEPENDS
-# @app.get("/heroes/{hero_id}", response_model=HeroPublic, status_code=status.HTTP_200_OK)
-# def read_hero(hero_id: int):
-#     with Session(engine) as session:
-#         hero = session.get(Hero, hero_id)
-#         if not hero:
-#             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Hero not found")
-#         return hero
-
-# WITH DEPENDS
-@app.get('/heroes/{hero_id}', response_model=HeroPublic, status_code=status.HTTP_200_OK)
+@app.get('/heroes/{hero_id}', response_model=HeroPublicWithTeam, status_code=status.HTTP_200_OK)
 def read_hero(*, session: Session = Depends(get_session), hero_id: int):
     hero = session.get(Hero, hero_id)
     if not hero:
@@ -160,27 +130,6 @@ def read_hero(*, session: Session = Depends(get_session), hero_id: int):
     
 
 # UPDATE
-
-# NO DEPENDS
-# @app.patch("/heroes/{hero_id}", response_model=HeroPublic, status_code=status.HTTP_200_OK)
-# def update_hero(hero_id: int, hero: HeroUpdate):
-#     with Session(engine) as session:
-#         db_hero = session.get(Hero, hero_id)
-#         if not db_hero:
-#             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Hero not found")
-#         hero_data = hero.model_dump(exclude_unset=True)
-#         extra_data = {}
-#         if 'password' in hero_data:
-#             password = hero_data['password']
-#             hashed_password = hash_password(password)
-#             extra_data['hashed_password'] = hashed_password
-#         db_hero.sqlmodel_update(hero_data)
-#         session.add(db_hero)
-#         session.commit()
-#         session.refresh(db_hero)
-#         return db_hero
-
-# WITH DEPENDS
 @app.patch('/heroes/{hero_id}', response_model=HeroPublic, status_code=status.HTTP_202_ACCEPTED)
 def update_hero(*, session: Session = Depends(get_session), hero_id: int, hero: HeroUpdate):
     db_hero = session.get(Hero, hero_id)
@@ -198,10 +147,7 @@ def update_hero(*, session: Session = Depends(get_session), hero_id: int, hero: 
         extra_data['hashed_password'] = hashed_password
         
     db_hero.sqlmodel_update(hero_data)
-    
-    # for key, value in hero_data.items():
-    #     setattr(db_hero, key, value)
-    
+
     session.add(db_hero)
     session.commit()
     session.refresh(db_hero)
@@ -210,19 +156,6 @@ def update_hero(*, session: Session = Depends(get_session), hero_id: int, hero: 
     
 
 # DELETE
-# NO DEPENDS
-# @app.delete('/heroes/{hero_id}')
-# def delete_hero(hero_id: int):
-#     with Session(engine) as session:
-#         hero = session.get(Hero, hero_id)
-#         if not hero:
-#             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Hero not found')
-#         session.delete(hero)
-#         session.commit()
-#         message = 'Hero deleted successfully!'
-#         return {'message': message}
-
-# WITH DEPENDS
 @app.delete('/heroes/{hero_id}')
 def delete_hero(*, session: Session = Depends(get_session), hero_id: int):
     hero = session.get(Hero, hero_id)
@@ -233,30 +166,6 @@ def delete_hero(*, session: Session = Depends(get_session), hero_id: int):
     message = 'Hero deleted successfully'
     return {'message': message}
 
-# def select_heroes():
-#     with Session(engine) as session:
-#         statement_1 = select(Hero)
-#         results = session.exec(statement_1)
-#         heroes = results.all()
-#         Compact
-#         heroes = session.exec(select(Hero)).all()
-#         print(heroes)
-#         List
-#         for hero in results:
-#             print(hero)
-#         Filter
-#         statement_2 = select(Hero).where(Hero.name == "DeadPond")
-#         one_hero = session.exec(statement_2)
-#         for data in one_hero:
-#             print(data)
-        
-
-# def main():
-#     select_heroes()
-    
-
-# if __name__ == "__main__":
-#     main()
 
 # CREATE
 @app.post('/teams/', response_model=TeamPublic, status_code=status.HTTP_201_CREATED)
@@ -273,3 +182,41 @@ def create_team(*, session: Session = Depends(get_session), team: TeamCreate):
 def read_teams(session: Session = Depends(get_session), offset: int = 0, limit: int = Query(default=100, le=100)):
     teams = session.exec(select(Team).offset(offset).limit(limit)).all()
     return teams
+
+
+@app.get('/teams/{team_id}', response_model=TeamPublicWithHeroes, status_code=status.HTTP_200_OK)
+def read_team(*, session: Session = Depends(get_session), team_id: int):
+    team = session.get(Team, team_id)
+    if not team:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Team not found')
+    return team
+
+
+# UPDATE
+@app.patch('/teams/{team_id}', response_model=TeamPublic, status_code=status.HTTP_202_ACCEPTED)
+def update_team(*, session: Session = Depends(get_session), team_id: int, team: TeamUpdate):
+    db_team = session.get(Team, team_id)
+    
+    if not db_team:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Team not found')
+    
+    team_data = team.model_dump(exclude_unset=True)
+    db_team.sqlmodel_update(team_data)
+    
+    session.add(db_team)
+    session.commit()
+    session.refresh(db_team)
+    
+    return db_team
+
+
+# DELETE
+@app.delete('/teams/{team_id}')
+def delete_team(*, session: Session = Depends(get_session), team_id: int):
+    team = session.get(Team, team_id)
+    if not team:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Team not found')
+    session.delete(team)
+    session.commit()
+    message = 'Team deleted successfully'
+    return {'message': message}
